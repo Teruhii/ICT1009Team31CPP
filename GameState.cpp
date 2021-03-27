eb0de378edd6f91cffc2ac8ac6c82ee19ec691a5
IVIillionaire <72209857+IVIillionaire@users.noreply.github.com>
2021-03-27 17:39:30 +0800

    Land files
    Files could not be ran as it still requires different files such as character, etc...

--- GameState.cpp
+++ GameState.cpp
@@ -1,25 +1,85 @@
-#include "stdafx.h"
-#include "GameState.h"
+#pragma once
 
-Void GameState::initView()
+#include <sstream>
+#include "DEFINITIONS.hpp"
+#include "GameState.hpp"
+
+#include <iostream>
+
+namespace Sonar
 {
-
-}
-
-void GameState::initKeybinds()
-{
-	std::ifstream ifs("Config/gamestate_keybinds.ini");
-
-	if (ifs.is_open())
+	GameState::GameState(GameDataRef data) : _data(data)
 	{
-		std::string key = "";
-		std::string key2 = "";
 
-		while (ifs >> key >> key2)
+	}
+
+	void GameState::Init()
+	{
+		this->_data->assets.LoadTexture("Game Background", GAME_BACKGROUND_FILEPATH);
+		this->_data->assets.LoadTexture("Pipe Up", PIPE_UP_FILEPATH);
+		this->_data->assets.LoadTexture("Pipe Down", PIPE_DOWN_FILEPATH);
+		this->_data->assets.LoadTexture("Land", LAND_FILEPATH);
+		this->_data->assets.LoadTexture("Bird Frame 1", BIRD_FRAME_1_FILEPATH);
+		this->_data->assets.LoadTexture("Bird Frame 2", BIRD_FRAME_2_FILEPATH);
+		this->_data->assets.LoadTexture("Bird Frame 3", BIRD_FRAME_3_FILEPATH);
+		this->_data->assets.LoadTexture("Bird Frame 4", BIRD_FRAME_4_FILEPATH);
+
+		pipe = new Pipe(_data);
+		land = new Land(_data);
+		bird = new Bird(_data);
+
+		_background.setTexture(this->_data->assets.GetTexture("Game Background"));
+	}
+
+	void GameState::HandleInput()
+	{
+		sf::Event event;
+
+		while (this->_data->window.pollEvent(event))
 		{
-			this->keybinds[key] = this->supportedKeys->at(key2);
+			if (sf::Event::Closed == event.type)
+			{
+				this->_data->window.close();
+			}
 
+			if (this->_data->input.IsSpriteClicked(this->_background, sf::Mouse::Left, this->_data->window))
+			{
+				bird->Tap();
+			}
+		}
+	}
+
+	void GameState::Update(float dt)
+	{
+		pipe->MovePipes(dt);
+		land->MoveLand(dt);
+
+		if (clock.getElapsedTime().asSeconds() > PIPE_SPAWN_FREQUENCY)
+		{
+			pipe->RandomisePipeOffset();
+
+			pipe->SpawnInvisiblePipe();
+			pipe->SpawnBottomPipe();
+			pipe->SpawnTopPipe();
+
+			clock.restart();
 		}
 
+		bird->Animate(dt);
+
+		bird->Update(dt);
+	}
+
+	void GameState::Draw(float dt)
+	{
+		this->_data->window.clear( sf::Color::Red );
+
+		this->_data->window.draw(this->_background);
+
+		pipe->DrawPipes();
+		land->DrawLand();
+		bird->Draw();
+
+		this->_data->window.display();
 	}
 }
