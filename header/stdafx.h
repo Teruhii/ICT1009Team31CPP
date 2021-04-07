c284a15835df623733f30279225360b6809773c0
Guardians <guancong96@gmail.com>
2021-04-07 21:24:31 +0800

    Removed OutFiles
    - Removed outerfile

--- header/stdafx.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <iostream>
-#include <ctime>
-#include <SFML/Graphics.hpp>
-#include <SFML/System.hpp>
-#include <SFML/Window.hpp>
-#include <SFML/Audio.hpp>
-#include <SFML/Network.hpp>
-#include "../header/Definition.h"
--- src/ObstacleManager.cpp
+++
@@ -1,7 +0,0 @@
-#include "../header/ObstacleManager.h"
-
-
-
-
-
-
